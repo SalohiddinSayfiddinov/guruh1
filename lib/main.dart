b40f73f7fa8079b0522eda5fe15ceef67b6fd177
SalohiddinSayfiddinov <95919972+SalohiddinSayfiddinov@users.noreply.github.com>
2026-03-25 18:27:30 +0500

    Add comment in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:guruh1/features/countries/presentation/provider/country_provider
 import 'package:provider/provider.dart';
 
 void main() {
+  // Edit qilindi
   runApp(const MyApp());
 }
 
